aba5cff3309d503a2f9493bfe46322a5886827c8
Joshua Nicholson <jsnicholson08@gmail.com>
2024-07-27 19:37:30 +0100

    set up data project with basic entities

---
+++ tagger_core/Data/Entity/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entity {
+    /// <summary>
+    /// individual tag to be applied to files
+    /// </summary>
+    [Table("Tag")]
+    public class Tag {
+        [Key]
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ tagger_core/Data/Repository/GenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repository {
+    public class GenericRepository<T> where T : class {
+        protected DbContext _context;
+
+        IEnumerable<T> GetAll() {
+
+        }
+    }
+}
