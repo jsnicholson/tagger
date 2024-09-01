c362786278ff2f7677993b4ef2210c990d82c8ce
Joshua Nicholson <jsnicholson08@gmail.com>
2024-09-01 22:58:38 +0100

    first draft of webapp
    API to return file paths, endpoint to return full files (just media rn)
    
    webapp to display grid of results, load more on button press

---
+++ TaggerApp/Api/Controllers/FileController.cs
@@ -0,0 +1,60 @@
+﻿using HeyRed.Mime;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileController : ControllerBase {
+        private const string _fileFolderPath = "D:\\GitRepos\\tagger\\TaggerApp\\Api\\Resources\\Example";
+
+        [HttpGet]
+        public IActionResult GetFilePaths(int startIndex = 0, int count = 20) {
+            var images = Directory.GetFiles(_fileFolderPath)
+                .Skip(startIndex)
+                .Take(count)
+                .Select(Path.GetFileName)
+                .ToList();
+
+            return Ok(images);
+        }
+
+        [HttpGet]
+        [Route("absolute")]
+        public IActionResult GetFilePathsAbsolute(int startIndex = 0, int count = 20) {
+            var baseUrl = $"{Request.Scheme}://{Request.Host}/api/file";
+
+            var images = Directory.GetFiles(_fileFolderPath)
+                .Skip(startIndex)
+                .Take(count)
+                .Select(file => $"{baseUrl}/{Path.GetFileName(file)}")
+                .ToList();
+
+            return Ok(images);
+        }
+
+        [HttpGet("{fileName}")]
+        public IActionResult GetFile(string fileName) {
+            // Build the full path to the image
+            var filePath = Path.Combine(_fileFolderPath, fileName);
+
+            // Check if the file exists
+            if (!System.IO.File.Exists(filePath)) {
+                return NotFound();
+            }
+
+            // Open the file stream
+            var file = System.IO.File.OpenRead(filePath);
+
+            // Determine the content type (MIME type)
+            var contentType = GetContentType(fileName);
+
+            // Return the file stream with the correct content type
+            return File(file, contentType);
+        }
+
+        private string GetContentType(string fileName) {
+            return MimeTypesMap.GetMimeType(fileName);
+        }
+    }
+}
---
+++ TaggerApp/WebApp/wwwroot/js/app.js
@@ -0,0 +1,18 @@
+﻿console.log("app.js run");
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("app.js dom loaded");
+    InitMasonry();
+});
+
+function InitMasonry() {
+    console.log("init masonry");
+    var grid = document.querySelector('#masonry-grid');
+
+    if (grid == null) return;
+
+    var msnry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        //columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+}
